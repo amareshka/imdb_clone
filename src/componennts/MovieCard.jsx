1cadd5d3928a4f44b47dd255d1879cebc681c6fa
amareshka <amareshbevoor@gmail.com>
2025-03-10 14:04:33 +0530

    Updated the data for movies

--- src/componennts/MovieCard.jsx
+++ src/componennts/MovieCard.jsx
@@ -1,7 +1,7 @@
-function MovieCard( id,title,poster){
+function MovieCard(id,name,poster){
     return(
-        <div key={id} className="bg-cover bg-center w-[150px] m-2 h-[40vh] rounded-xl flex items-end hover:scale-[1.1] hover:cursor-pointer duration-300" style={{backgroundImage: `${poster}` }}>
-            <div className="p-2 bg-red text-center w-full text-white">{title}</div>
+        <div key={id} id='movie-`${id}`' className="bg-cover bg-center w-[180px] m-2 h-[40vh] rounded-xl border flex items-end hover:scale-[1.1] hover:cursor-pointer duration-300" style={{backgroundImage: `url(https://m.media-amazon.com/images/I/71Jxq2p5YWL._AC_UF1000,1000_QL80_.jpg)` }}>
+            <div className="p-2 bg-gray text-center w-full text-white">{name}{poster}</div>
         </div>
     )
 }
--- src/componennts/Movies.jsx
+++ src/componennts/Movies.jsx
@@ -5,46 +5,24 @@ import axios from "axios";
 function Movies() {
   const [moviesArr, setMovies] = useState([]);
 
-  //axios.get("https://api.allorigins.win/get?url=https://freetestapi.com/api/v1/movies")
-  // useEffect(()=>{
-  //   axios.get("https://api.allorigins.win/get?url=https://freetestapi.com/api/v1/movies").then(function(data){
-  //     console.log(data.data.contents);
-  //     //console.log(data)
-  //     setMovies(data.data.contents)
-  //   });
-  // },[]);
-
   useEffect(() => {
     const fetchMoviesData = async () => {
       const moviesData = await axios.get("https://api.allorigins.win/get?url=https://freetestapi.com/api/v1/movies");
-      //const moviesJson = await JSON.stringify(moviesData);
-      //console.log(moviesData.data.contents);
-      if (moviesData?.data?.contents) {
-        setMovies([moviesData]);
-      }
-
+      const moviesJson = JSON.parse(moviesData?.data.contents);
+     
+      setMovies(moviesJson)
     };
     fetchMoviesData();
   }, []);
-
-  // useEffect(() => {
-  //   fetch(`https://api.allorigins.win/get?url=https://freetestapi.com/api/v1/movies`)
-  //     .then(response => response.json())
-  //     .then(response => {  console.log(response.contents)     
-  //       setMovies([response.contents])
-  //   })
-  // },[])
-
-
-
 
   return (
     <>
       <div className="text-xl font-bold text-center m-5">Trending Movies</div>
       <div className="flex flex-row flex-wrap justify-around gap-8">
-
-        {moviesArr.map((movie) => {
-          return <MovieCard key={movie.id} name={movie.title} poster={movie.poster} />
+        {console.log(moviesArr)}
+        {moviesArr.map((movie, index) => {
+          
+          return <MovieCard key={index} name={movie.title} poster={movie.poster} />
         })}
 
       </div>
